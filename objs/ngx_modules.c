748b4a7ab43af4667b7521fc54fa29fe63602be4
wujiang1 <wujiang1@staff.sina.com.cn>
2014-12-24 10:51:37 +0800

    标注nginx_module类型

--- objs/ngx_modules.c
+++ objs/ngx_modules.c
@@ -35,35 +35,35 @@ extern ngx_module_t  ngx_http_range_body_filter_module;
 extern ngx_module_t  ngx_http_not_modified_filter_module;
 
 ngx_module_t *ngx_modules[] = {
-    &ngx_core_module,  //core
-    &ngx_errlog_module, //core
-    &ngx_conf_module,
-    &ngx_events_module, //core
-    &ngx_event_core_module,
-    &ngx_epoll_module,
-    &ngx_http_module,   //core
-    &ngx_http_core_module,
-    &ngx_http_log_module, //http
-    &ngx_http_upstream_module,
-    &ngx_http_static_module,
-    &ngx_http_index_module,
-    &ngx_http_limit_req_module,
-    &ngx_http_map_module,
-    &ngx_http_split_clients_module,
-    &ngx_http_referer_module,
-    &ngx_http_rewrite_module,
-    &ngx_http_proxy_module,
-    &ngx_http_browser_module,
-    &ngx_http_upstream_ip_hash_module,
-    &ngx_http_portrait_module,
-    &ngx_http_write_filter_module,
-    &ngx_http_header_filter_module,
-    &ngx_http_chunked_filter_module,
-    &ngx_http_range_header_filter_module,
-    &ngx_http_headers_filter_module,
-    &ngx_http_copy_filter_module,
-    &ngx_http_range_body_filter_module,
-    &ngx_http_not_modified_filter_module,
+    &ngx_core_module,           //NGX_CORE_MODULE
+    &ngx_errlog_module,          //NGX_CORE_MODULE
+    &ngx_conf_module,           //NGX_CONF_MODULE
+    &ngx_events_module,         //NGX_CORE_MODULE
+    &ngx_event_core_module,     //NGX_EVENT_MODULE
+    &ngx_epoll_module,           //NGX_EVENT_MODULE
+    &ngx_http_module,            //NGX_CORE_MODULE
+    &ngx_http_core_module,       //NGX_HTTP_MODULE
+    &ngx_http_log_module,        //NGX_HTTP_MODULE
+    &ngx_http_upstream_module,  //NGX_HTTP_MODULE
+    &ngx_http_static_module,      //NGX_HTTP_MODULE
+    &ngx_http_index_module,      //NGX_HTTP_MODULE
+    &ngx_http_limit_req_module,   //NGX_HTTP_MODULE
+    &ngx_http_map_module,       //NGX_HTTP_MODULE
+    &ngx_http_split_clients_module,        //NGX_HTTP_MODULE
+    &ngx_http_referer_module,             //NGX_HTTP_MODULE
+    &ngx_http_rewrite_module,             //NGX_HTTP_MODULE
+    &ngx_http_proxy_module,              //NGX_HTTP_MODULE
+    &ngx_http_browser_module,            //NGX_HTTP_MODULE
+    &ngx_http_upstream_ip_hash_module,   //NGX_HTTP_MODULE
+    &ngx_http_portrait_module,             //NGX_HTTP_MODULE
+    &ngx_http_write_filter_module,          //NGX_HTTP_MODULE
+    &ngx_http_header_filter_module,        //NGX_HTTP_MODULE
+    &ngx_http_chunked_filter_module,       //NGX_HTTP_MODULE
+    &ngx_http_range_header_filter_module,  //NGX_HTTP_MODULE
+    &ngx_http_headers_filter_module,       //NGX_HTTP_MODULE
+    &ngx_http_copy_filter_module,          //NGX_HTTP_MODULE
+    &ngx_http_range_body_filter_module,    //NGX_HTTP_MODULE
+    &ngx_http_not_modified_filter_module,   //NGX_HTTP_MODULE
     NULL
 };
 
